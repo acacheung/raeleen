c765d5d87f72656db7db6da97235d73db6da11dd
acacheung <acacheung@gmail.com>
2013-10-20 01:50:35 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Drawing.create(title: "For A Little While", image: "for_a_little_while.jpg", thumbnail: "for_a_little_while_thumb.jpg")
+Drawing.create(title: "Medea", image: "medea.jpg", thumbnail: "medea_thumb.jpg")
+Drawing.create(title: "Pretty Maids II", image: "pretty_maids_2.jpg", thumbnail: "pretty_maids_2_thumb.jpg")
+Drawing.create(title: "Pretty Maids III", image: "pretty_maids_3.jpg", thumbnail: "pretty_maids_3_thumb.jpg")
+Drawing.create(title: "Pretty Maids", image: "pretty_maids.jpg", thumbnail: "pretty_maids_thumb.jpg")
+Drawing.create(title: "Untitled II", image: "untitled_2.jpg", thumbnail: "untitled_2_thumb.jpg")
+Drawing.create(title: "Untitled III", image: "untitled_3.jpg", thumbnail: "untitled_3_thumb.jpg")
+Drawing.create(title: "Untitled V", image: "untitled_5.jpg", thumbnail: "untitled_5_thumb.jpg")
+Drawing.create(title: "Untitled", image: "untitled.jpg", thumbnail: "untitled_thumb.jpg")
+
+Print.create(title: "Haunted Chamber", image: "haunted_chamber.jpg", thumbnail: "haunted_chamber_thumb.jpg")
+Print.create(title: "More Beautiful In Death", image: "more_beautiful_in_death.jpg", thumbnail: "more_beautiful_in_death_thumb.jpg")
+Print.create(title: "Perfect Grave", image: "perfect_grave.jpg", thumbnail: "perfect_grave_thumb.jpg")
+Print.create(title: "Father's Pride", image: "fathers_pride.jpg", thumbnail: "fathers_pride_thumb.jpg")
+Print.create(title: "Mother's Treasure", image: "mothers_treasure.jpg", thumbnail: "mothers_treasure_thumb.jpg")
+Print.create(title: "Inhospitable", image: "inhospitable.jpg", thumbnail: "inhospitable_thumb.jpg")
